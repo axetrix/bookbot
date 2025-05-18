8470817be5f9bf8b957ce546c1ce697054e7bd3c
bear <bear@arctic.land>
2025-05-18 11:40:21 -0500

    Add some codefixes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-from stats import get_num_words, get_characters_count, get_sorted_char_list, print_counts_dict
+from stats import get_num_words, get_characters_count, get_sorted_char_list
 
 def get_book_text(filepath: str) -> str:
     file_contents = ""
@@ -12,25 +12,39 @@ def get_book_text(filepath: str) -> str:
 
     return file_contents
 
-def main(file_path):
-    content = get_book_text(file_path)
-    num_words = get_num_words(content)
-    character_dict = get_characters_count(content);
-
+def print_stats(file_path: str, num_words: int, sorted_list: list[dict]):
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {file_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    print_counts_dict(get_sorted_char_list(character_dict))
+    for item in sorted_list:
+        character = item.get("char")
+
+        if character.isalpha():
+            print(f"{character}: {item.get("num")}")
+            
     print("============= END ===============")
 
-if len(sys.argv) < 2:
-    print("Usage: python3 main.py <path_to_book>")
-    sys.exit(1)
 
-if os.path.exists(sys.argv[1]):
-    main(sys.argv[1])
-else:
-    print("Please send a file as an argument")
-    sys.exit(1)
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    if not os.path.exists(sys.argv[1]):
+        print("Please send a file as an argument")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+        
+    content = get_book_text(file_path)
+
+    num_words = get_num_words(content)
+    character_dict = get_characters_count(content)
+    sorted_list = get_sorted_char_list(character_dict)
+
+    print_stats(file_path, num_words, sorted_list)
+
+
+main()
--- stats.py
+++ stats.py
@@ -20,13 +20,8 @@ def get_sorted_char_list(c_dict: dict[str, int]) -> dict[str, int]:
     list = []
 
     for char_lower, num in c_dict.items():
-        if char_lower.isalpha():
-            list.append({"char": char_lower, "num": num})
+        list.append({"char": char_lower, "num": num})
 
     list.sort(key=sort_on, reverse=True)
 
     return list
-
-def print_counts_dict(list: list[dict[str, int|str]]):
-    for item in list:
-        print(f"{item.get("char")}: {item.get("num")}")
